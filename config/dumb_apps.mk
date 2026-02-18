1ed60741db69b19b1d3bae427e63151887197876
inturvert <irkarharsharaj@gmail.com>
2026-02-18 16:12:36 +0530

    Added DumbOs specific dumb_apps.mk file to remove and include prebuilt apps

---
+++ config/dumb_apps.mk
@@ -0,0 +1,13 @@
+#App Removal
+PRODUCT_PACKAGES -= \
+	Jelly \
+	Recorder \
+	Updater \
+	AudioFX \
+	Seedvault 
+
+# Including Prebuilt Apps
+PRODUCT_PACKAGES += \
+	Spotify \
+	Niagara \
+	WhatsApp
